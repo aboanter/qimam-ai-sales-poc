7389dc0b8def61a142ed9a1361da62e09028cb32
aboanter <43633167+aboanter@users.noreply.github.com>
2026-09-07 21:08:40 +0300

    Load background job client

--- public/design-system-v30.js
+++ public/design-system-v30.js
@@ -39,4 +39,5 @@
   loadScript('/section-design-v32.js','qimam-section-v32');
   loadScript('/section-renderer-v33.js','qimam-section-v33');
   loadScript('/insight-renderer-v331.js','qimam-insight-v331');
+  loadScript('/background-jobs-v1.js','qimam-background-jobs-v1');
 })();
